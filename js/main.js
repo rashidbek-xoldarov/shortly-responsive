5c302a0ca66100f38fa6dd7852ba32f57182c6f6
rashid1111111 <rashidbek0309@gmail.com>
2022-08-21 16:28:23 +0500

    Finish

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const elNavbarBtn = document.querySelector(".navbar-btn");
+const elNavbar = document.querySelector(".navbar");
+
+elNavbarBtn.addEventListener("click", function () {
+  elNavbar.classList.toggle("navbar-open");
+});
+
+elNavbar.addEventListener("click", function () {
+  elNavbar.classList.toggle("navbar-open");
+});
